c3ae2c1106cfc3ddd145af145e1b3a73c1cb3f7f
dainty06 <queeniejpongase@gmail.com>
2026-07-24 14:43:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Welcome to DaintyDishCo!");
+const mugs = [
+    {
+        image: "mug.jpg",
+        title: "Ceramic Mug",
+        price: "₱360"
+    },
+    {
+        image: "mug2.jpg",
+        title: "Lady Ceramic Mug",
+        price: "₱480"
+    }
+];
+
+let currentMug = 0;
+
+function changeMug() {
+    currentMug = (currentMug + 1) % mugs.length;
+
+    document.getElementById("mugImage").src = mugs[currentMug].image;
+    document.getElementById("mugTitle").textContent = mugs[currentMug].title;
+    document.getElementById("mugPrice").textContent = mugs[currentMug].price;
+}
